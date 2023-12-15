1eef7757e7734f73e2c4d8889882bf2a46b28ee7
th-schmidt <thomas.schmidt@uni-mannheim.de>
2023-12-15 11:56:11 +0100

    Fix typo

--- Modul_1/Aufgabe2_shell/befehle.sh
+++ Modul_1/Aufgabe2_shell/befehle.sh
@@ -6,7 +6,7 @@ sed -E 's/IMPORTANT\t*//g' 2023-11-20-Article_list_dirty.tsv > clean_issn.tsv
 # Grep ISSN and Date column but preserve textline layout (i.e. omit grep -o parameter)
 cut -f 5,12 clean_issn.tsv | grep -E "[0-9]{4}[-0-9A-Z]*" > issn_dates.tsv
 
-# Remove ISSN strings from iss_dates.tsv and sort it uniquly; ignore non-printable chars
+# Remove ISSN strings from issn_dates.tsv and sort it uniquly; ignore non-printable chars
 sed -E 's/issn[:[:space:]]*//gI' issn_dates.tsv | sort -ui > 2023-11-30-ISSN_DATES_ths.tsv
 
 # Remove temp tsvs

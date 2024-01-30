8c1fa7bf4d19f4daa9cc329d76e3e52e560edf80
th-schmidt <thomas.schmidt@uni-mannheim.de>
2024-01-30 16:30:11 +0100

    Update PyTerrier notebooks and fix web_interface.py with correct Java env variable

--- Modul_2/pyterrier-tut/pyterrier_web_interface.py
+++ Modul_2/pyterrier-tut/pyterrier_web_interface.py
@@ -3,20 +3,22 @@
 import streamlit as st
 import pyterrier as pt
 import pickle
+import os
+
+os.environ["JAVA_HOME"] = "./jdk/Contents/Home/"
 
 if not pt.started():
     pt.init()
 
 def init():
-
-    index = pt.IndexFactory.of("/ai_index_mult/data.properties")
+    index = pt.IndexFactory.of("./makerspace_index_mult/data.properties")
     st.session_state["engine"] = pt.BatchRetrieve(index, wmodel="TF_IDF")
-    st.session_state["data"] = pickle.load(open("/workspace/ai_publications.pkl", "rb"))
+    st.session_state["data"] = pickle.load(open("./bibsonomy_clean_data/makerspace_vr.pkl", "rb"))
+    
 
 def search(query):
-
     res = st.session_state["engine"].search(query)
-    fields_to_show = ['text', 'tags', 'url', 'author', 'description']
+    fields_to_show = ['text', 'tags', 'url', 'author', 'abstract']
 
     for _, row in res.iterrows():
         score = round(row['score'], 2)
@@ -25,6 +27,16 @@ def search(query):
         for field in fields_to_show:
             if field == "text":
                 st.title(entry[field])
+            elif field == 'author' or field == 'tags':
+                if isinstance(entry[field], list):
+                    st.write(f"{field.capitalize()}: \t {', '.join(entry[field])}")
+                else:
+                    st.write(f"{field.capitalize()}: \t {entry[field]}")
+            elif field == 'url':
+                if len(entry[field]) <= 1:
+                    st.write(f"{field.upper()}: \t {entry['docno']}")
+                else:
+                    st.write(f"{field.upper()}: \t {entry[field]}")
             else:
                 st.write(f"{field.capitalize()}: \t {entry[field]}")
 
--- Modul_2/pyterrier-tut/tools/littlehelper.py
+++ Modul_2/pyterrier-tut/tools/littlehelper.py
@@ -1,17 +1,29 @@
 import requests
 import pandas as pd
+import re
 
-def df_from_bibsonomy_query(query, limit=1000):
+from pathlib import Path
+
+
+def df_from_bibsonomy_query(query, limit=1000, to_json=False, output_dir=Path('.'), output_filename=None):
     """
-    Create a DataFrame by querying BibSonomy for specific search terms.
+    Retrieves data from Bibsonomy based on a specified query and converts it into a pandas DataFrame. 
 
-    Args:
-        query (str or list of str): The search query or a list of search queries to retrieve data for.
-        limit (int, optional): The maximum number of items to retrieve for each query. Defaults to 1000.
+    Parameters:
+    - query (str or list of str): The search term(s) to query in Bibsonomy. Can be a single string or a list of strings.
+    - limit (int, optional): The maximum number of items to retrieve for each query. Defaults to 1000.
+    - to_json (bool, optional): If True, the resulting DataFrame is saved as a JSON file. Defaults to False.
+    - output_dir (Path, optional): The directory where the JSON file will be saved. Defaults to the current directory.
+    - output_filename (str, optional): The name of the JSON file to save the results in. If not specified, a default name based on the query will be used.
 
     Returns:
-        pandas.DataFrame: A DataFrame containing the retrieved data from BibSonomy, with an additional 'search_string' column
-        that indicates the corresponding search query for each row.
+    pandas.DataFrame: A DataFrame containing the retrieved data from Bibsonomy.
+
+    Notes:
+    - The function first validates the query format. If the query is not a string or list of strings, an error message is displayed.
+    - Data from Bibsonomy is fetched for each query term, and the results are combined into a single DataFrame.
+    - If 'to_json' is True, the DataFrame is saved as a JSON file in the specified directory with the given filename.
+    - The function prints the number of items retrieved for each query and the file path if data is saved as JSON.
     """
     # Check datatype of query
     if not isinstance(query, list):
@@ -19,6 +31,7 @@ def df_from_bibsonomy_query(query, limit=1000):
             query = [query]
         else:
             print('Please provide a valid query (string or list of strings).')
+            
     # Query bibsonomy
     dfs = []
     for q in query:
@@ -32,6 +45,24 @@ def df_from_bibsonomy_query(query, limit=1000):
             dfs.append(df)
         else:
             print(f'{response.status_code}: {url} is not available.')
+            
     # Concat DataFrames
     df = pd.concat(dfs, ignore_index=True)
+    
+    # If to_json == True save df as .json 
+    if to_json == True:
+        if not isinstance(output_dir, Path):
+            output_dir = Path(output_dir)
+            if not output_dir.exists():
+                output_dir.mkdir(parents=True)
+        if output_filename is not None:
+            if not output_filename.endswith('.json'):
+                output_filename = Path(f"{output_dir}/{output_filename.replace(' ', '_').lower()}.json")
+            else:
+                output_filename = output_dir.joinpath(output_filename.replace(' ', '_').lower())
+        else:
+            output_filename = output_dir.joinpath(f'bibsonomy_{query[0]}.json')
+        df.to_json(output_filename, orient='records')
+        print(f'\nSaved data to "{output_filename}"')
+        
     return df

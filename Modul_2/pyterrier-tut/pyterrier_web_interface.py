4b93eb5617d09f3ccf074aae21b50321b9bbf43e
th-schmidt <thomas.schmidt@uni-mannheim.de>
2024-01-31 11:36:26 +0100

    Add slider for limit search results in web interface

--- Modul_2/pyterrier-tut/pyterrier_web_interface.py
+++ Modul_2/pyterrier-tut/pyterrier_web_interface.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 import pyterrier as pt
+import numpy as np
 import pickle
 import os
 
@@ -15,38 +16,45 @@ def init():
     index = pt.IndexFactory.of("./makerspace_index_mult/data.properties")
     st.session_state["engine"] = pt.BatchRetrieve(index, wmodel="TF_IDF")
     st.session_state["data"] = pickle.load(open("./bibsonomy_clean_data/makerspace_vr.pkl", "rb"))
-    
 
-def search(query):
-    res = st.session_state["engine"].search(query)
+
+def search(query, search_limit):
+    
+    res = st.session_state["engine"].search(query)[:search_limit]
     fields_to_show = ['text', 'tags', 'url', 'author', 'abstract']
+    result_num = 1
 
     for _, row in res.iterrows():
         score = round(row['score'], 2)
         entry = st.session_state["data"][st.session_state["data"]['docno'] == row['docno']].iloc[0]
-
+        
+        st.write(f'Result **{result_num}** of **{len(res)}**')
+        
         for field in fields_to_show:
             if field == "text":
                 st.title(entry[field])
             elif field == 'author' or field == 'tags':
                 if isinstance(entry[field], list):
-                    st.write(f"{field.capitalize()}: \t {', '.join(entry[field])}")
+                    st.write(f"**{field.capitalize()}:** \t {', '.join(entry[field]).strip()}")
                 else:
                     st.write(f"{field.capitalize()}: \t {entry[field]}")
             elif field == 'url':
                 if len(entry[field]) <= 1:
-                    st.write(f"{field.upper()}: \t {entry['docno']}")
+                    st.write(f"**{field.upper()}:** \t {entry['docno']}")
                 else:
-                    st.write(f"{field.upper()}: \t {entry[field]}")
+                    st.write(f"**{field.upper()}:** \t {entry[field]}")
             else:
-                st.write(f"{field.capitalize()}: \t {entry[field]}")
+                st.write(f"**{field.capitalize()}:** \t {entry[field]}")
 
-        st.write(f"Score: {score}")
+        st.write(f"**:red[Score: {score}]**")
         st.divider()
+        
+        result_num += 1
 
 
 if not "engine" in st.session_state:
     init()
 
-query = st.sidebar.text_input("Query")
-st.sidebar.button("Search", on_click=search, args=(query,))
+query = st.sidebar.text_input("**Query**")
+slider_value = st.sidebar.slider(label="**Number of Search Results**", min_value=1, max_value=500, value=100)
+st.sidebar.button("Search", on_click=search, args=(query, slider_value))

91a79dd98e0cccb5154c2818971b01b11e24c810
th-schmidt <thomas.schmidt@uni-mannheim.de>
2024-02-01 08:51:13 +0100

    Rework web_interface: Add filter section and more flags

--- Modul_2/pyterrier-tut/pyterrier_web_interface.py
+++ Modul_2/pyterrier-tut/pyterrier_web_interface.py
@@ -19,55 +19,55 @@ def init():
     st.session_state["lexicon"] = index.getLexicon()
 
 
-def search(query, search_limit, abstract_flag, url_flag):
-    res = st.session_state["engine"].search(query)[:search_limit]
-    fields_to_show = ['text', 'tags', 'url', 'author', 'abstract']
+def print_results(entries):
     result_num = 1
-    
-    # Check for flags and gather entries from _res_
-    if abstract_flag:
-        entries = []
-        for _, row in res.iterrows():
-            score = round(row['score'], 2)
-            entry = st.session_state["data"][st.session_state["data"]['docno'] == row['docno']].iloc[0]
-            if entry['abstract'] != None:
-                entries.append((entry, score))
-    elif url_flag:
-        entries = []
-        for _, row in res.iterrows():
-            score = round(row['score'], 2)
-            entry = st.session_state["data"][st.session_state["data"]['docno'] == row['docno']].iloc[0]
-            if not len(entry['url']) <= 1:
-                entries.append((entry, score))
-    else:
-        entries = []            
-        for _, row in res.iterrows():
-            score = round(row['score'], 2)
-            entry = st.session_state["data"][st.session_state["data"]['docno'] == row['docno']].iloc[0]
-            entries.append((entry, score))
+    fields_to_show = ['text', 'year', 'author', 'url', 'abstract', 'tags']
+    if len(entries) >= 1:
+        for entry in entries:
+            st.write(f'Result **{result_num}** of **{len(entries)}**')
             
-    # Print entries        
-    for entry in entries:
-        st.write(f'Result **{result_num}** of **{len(entries)}**')
-        
-        for field in fields_to_show:
-            if field == "text":
-                st.title(entry[0][field])
-            elif field == 'author' or field == 'tags':
-                if isinstance(entry[0][field], list):
-                    st.write(f"**{field.capitalize()}:** \t {', '.join(entry[0][field]).strip()}")
+            for field in fields_to_show:
+                if field == "text":
+                    st.title(entry[0][field])
+                elif field == 'author' or field == 'tags':
+                    if isinstance(entry[0][field], list):
+                        st.write(f"**{field.capitalize()}:** \t {', '.join(entry[0][field]).strip()}")
+                    else:
+                        st.write(f"{field.capitalize()}: \t {entry[0][field]}")
+                elif field == 'url':
+                    if len(entry[0][field]) <= 1:
+                        st.write(f"**{field.upper()}:** \t {entry[0]['docno']}")
+                    else:
+                        st.write(f"**{field.upper()}:** \t {entry[0][field]}")
                 else:
-                    st.write(f"{field.capitalize()}: \t {entry[0][field]}")
-            elif field == 'url':
-                if len(entry[0][field]) <= 1:
-                    st.write(f"**{field.upper()}:** \t {entry[0]['docno']}")
-                else:
-                    st.write(f"**{field.upper()}:** \t {entry[0][field]}")
-            else:
-                st.write(f"**{field.capitalize()}:** \t {entry[0][field]}")
-        st.write(f"**:blue[Score: {entry[1]}]**")
-        st.divider()
-        result_num += 1        
+                    st.write(f"**{field.capitalize()}:** \t {entry[0][field]}")
+            st.write(f"**:blue[Score: {entry[1]}]**")
+            st.divider()
+            result_num += 1
+    else:
+        st.write(f'No entries found.')
+        st.write(f'Please adjust your search criteria. :pray:')
+
+
+def search(query, search_limit, year_range, abstract_flag, url_flag):
+    res = st.session_state["engine"].search(query)[:search_limit]
+    
+    filtered_entries = []
+    
+    for _, row in res.iterrows():
+        score = round(row['score'], 2)
+        entry = st.session_state["data"][st.session_state["data"]['docno'] == row['docno']].iloc[0]
+
+        if entry['year'] >= year_range[0] and entry['year'] <= year_range[1]:
+            include_entry = True
+            if abstract_flag:
+                include_entry = include_entry and entry['abstract'] != None
+            if url_flag:
+                include_entry = include_entry and len(entry['url']) > 1
+            if include_entry:
+                filtered_entries.append((entry, score))
+            
+    print_results(filtered_entries)
 
 
 def top_search_terms():
@@ -86,13 +86,15 @@ if not "engine" in st.session_state:
 ### Sidebar: Searching
 st.sidebar.title(f'**Search**')
 query = st.sidebar.text_input(f'**Query:**', value='makerspace')
-slider_value = st.sidebar.slider(label="**Number of Search Results:**", min_value=1, max_value=500, value=100)
 
-# Checkboxes / Flags
-abstract_flag = st.sidebar.checkbox(label='Show only results with abstracts', value=False)
-url_flag = st.sidebar.checkbox(label='Show only results with unique URLs', value=False)
+# Sidebar filter section
+with st.sidebar.expander('Filters'):
+    year_range = st.select_slider(label='**Time period**', options=list(range(1980, 2025)), value=(1980, 2024))
+    search_limit = st.slider(label="**Number of Search Results**", min_value=1, max_value=500, value=250)
+    abstract_flag = st.checkbox(label='Show only results with abstracts', value=False)
+    url_flag = st.checkbox(label='Show only results with unique URLs', value=False)
 
-st.sidebar.button("Search", on_click=search, args=(query, slider_value, abstract_flag, url_flag))
+st.sidebar.button("Search", on_click=search, args=(query, search_limit, year_range, abstract_flag, url_flag))
 st.sidebar.divider()
 
 ### Sidebar: Statistics
